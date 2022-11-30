7090942fd972925f2e1ffef35df7503009f4ae5b
SuperSonic <supersonic@livemail.tw>
2022-11-30 14:01:03 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="logo" src="./assets/logo.png">
+    <HelloWorld/>
   </div>
 </template>
 
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  width: 120px;
 }
 </style>
